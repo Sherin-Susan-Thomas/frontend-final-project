274e73cdd6840288abc1b91ed3da1721a9517f3e
Jenfi72 <jennyfiskaare@Jennys-MacBook-Air.local>
2022-06-05 20:58:46 +0200

    added createstore and thunk

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom'
 import './index.css';
 import reducers from './reducers';
 import { Provider } from 'react-redux';
-import { applyMiddleware, compose} from 'redux';
+import { createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 
 import { App } from './App'
 import { configureStore } from '@reduxjs/toolkit';
 
-const store = configureStore (reducers, compose(applyMiddleware(thunk)))
+const store = createStore (reducers, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
 <Provider store={store}>

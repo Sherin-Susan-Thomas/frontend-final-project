423e5263f03c5e4b3cf0ac6c4959494e52742470
Jenfi72 <jennyfiskaare@Jennys-MacBook-Air.local>
2022-06-08 21:21:18 +0200

    css header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { TopBar } from 'components/topbar/TopBar'
+import { TopBar } from 'components/topbar/TopBar';
 import React from 'react'
+import { HomePage } from 'pages/home/Home';
 
 export const App = () => {
   return (
     <>
     <TopBar />
+    <HomePage />
 
     </>
   )
---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "components/header/Header";
+import "./home.css";
+
+export const HomePage= () => {
+return(
+    <div className="home">
+        <Header />
+Homepage
+
+    </div>
+)
+};

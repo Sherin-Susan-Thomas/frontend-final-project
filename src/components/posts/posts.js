b14b9a1860dc5d14f8f5879887423b7da13c4d60
Jenfi72 <jennyfiskaare@Jennys-MacBook-Air.local>
2022-06-13 23:10:41 +0200

    testing to fetch

--- src/components/posts/posts.js
+++ src/components/posts/posts.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./posts.css";
 import { Post } from "components/post/Post";
+
 export const Posts = () => {
   return (
     <div className="posts">
-      <Post />
+      {Posts.map(p=>(
+
+<Post post={p}/>
+      ))}
     </div>
   );
 };
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Header } from "components/header/Header";
 import { TopBar } from "components/topbar/TopBar";
@@ -6,14 +6,28 @@ import { TopBar } from "components/topbar/TopBar";
 import "./home.css";
 import { SideBar } from "components/sidebar/Sidebar";
 import { Posts } from "components/posts/posts";
+import axios from "axios";
+
+const API_URL="https://final-sprint.herokuapp.com/api/posts"
+
 export const HomePage = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() =>{
+const fetchPosts = async () => {
+ const res= axios.get(API_URL)
+  setPosts(res.data)
+}
+fetchPosts()
+  },[])
+
   return (
     <>
       <div className="home">
         <TopBar />
         <Header />
 
-        <Posts />
+        <Posts posts={posts}/>
         <SideBar />
       </div>
     </>

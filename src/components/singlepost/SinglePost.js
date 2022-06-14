2b8b239acc77c922bbb5b5e910e53daf2c4e4566
Jenfi72 <jennyfiskaare@Jennys-MacBook-Air.local>
2022-06-14 23:50:22 +0200

    changes to singlepost

--- src/components/singlepost/SinglePost.js
+++ src/components/singlepost/SinglePost.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./singlepost.css";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 
 export const SinglePost = () => {
+const location = useLocation()
+const path= location.pathname.split("/")[2];
+const [post, setPost] = useState({})
+
+useEffect(() =>{
+  const getPost = async () => {
+    const res = await axios.get("/posts/"+ path);
+    setPost(res.data)
+  };
+getPost()
+}, [path])
+
   return (
     <div className="singlePost">
       SINGLE POST
       <div className="singlePostWrapper">
-        <img
-          src="https://resebloggaren.se/wp-content/uploads/2020/02/rio-de-janeiro-1963744_1920.jpg"
+{post.photo &&
+  <img
+          src={post.photo}
           alt=""
           className="singlePostImg"
-        ></img>
+        />
+}
+
         <h1 className="singlePostTitle">
-          lorem ipsum lorem ipsum
+          {post.title}
           <div className="singlePostEdit">
             <i className="singlePostIcon">
               <span role="img" aria-label="image">
@@ -28,25 +45,13 @@ export const SinglePost = () => {
         </h1>
         <div className="singlePostInfo">
           <span className="singlepostAuthor">
-            <b>Author:**NAME**</b>
+            <b>{post.username}</b>
           </span>
+          <span className="singlePostDesc">{new Date(post.createdAt).toDateString()}</span>
         </div>
         <div className="singlePostDesc">
           <span className="singlepostdesc">
-            "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-            quae ab illo inventore veritatis et quasi architecto beatae vitae
-            dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-            aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-            eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
-            qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
-            sed quia non numquam eius modi tempora incidunt ut labore et dolore
-            magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis
-            nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut
-            aliquid ex ea commodi consequatur? Quis autem vel eum iure
-            reprehenderit qui in ea voluptate velit esse quam nihil molestiae
-            consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla
-            pariatur?"
+            {post.desc}
           </span>
         </div>
       </div>

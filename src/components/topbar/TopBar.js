d23f488e05f130d587976c93eb62024701f2f882
Sherin Susan Thomas <sherinsusanthomas95@gmail.com>
2022-06-21 13:10:02 +0200

    removed link

--- src/components/topbar/TopBar.js
+++ src/components/topbar/TopBar.js
@@ -8,59 +8,43 @@ export const TopBar = () => {
   const id = user._id;
   console.log("id", id);
 
-
-
-
-
   return (
     <div className="topBar">
-
-
-<div className="topLeft">
-<button className="logout">
-          <Link to="/" onClick={() => window.localStorage.clear()}>
+      <div className="topLeft">
+        <button className="logout">
+          <Link
+            to="/"
+            onClick={() => window.localStorage.clear()}
+            className="link"
+          >
             Logout
           </Link>
         </button>
-  
+
         <ul className="topList">
-          <Link to="/home">
+          <Link to="/home " className="link">
             {" "}
             <li className="topListItem">HOME</li>
           </Link>
-          <Link to="/newpost">
-            <li className="topListItem">
-              NEW POST
-            </li>
+          <Link to="/newpost" className="link">
+            <li className="topListItem">NEW POST</li>
           </Link>
           <li className="topListItem">FAVOURITES</li>
         </ul>
-        </div>
+      </div>
 
-  {/*     <div className="menu-btn">
-<div className="menu-btn_burger"></div>
-      </div> */}
-    
-      <div className="topCenter">
-
-     
- 
-        </div>
+      <div className="topCenter"></div>
 
       <div className="topRight">
         <div className="userInfo">
-        <Link to="/users/pic">
-          <img className="topImg" src={user.profilepicture} alt="" />
-        </Link>
-        <Link to={`/users/${id}`}>
-          <p className="profileName">{user.username}</p>{" "}
-        </Link>
+          <Link to="/users/pic" className="link">
+            <img className="topImg" src={user.profilepicture} alt="" />
+          </Link>
+          <Link to={`/users/${id}`} className="link">
+            <span className="profileName">{user.username}</span>{" "}
+          </Link>
         </div>
-     
-
-      
-        </div>
-
+      </div>
     </div>
   );
 };

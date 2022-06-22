c91bc17bb80206ffc8927a9cddb6b0c40a569d51
Sherin Susan Thomas <sherinsusanthomas95@gmail.com>
2022-06-22 10:36:25 +0200

    added about us

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -9,6 +9,7 @@ import { Posts } from "components/posts/posts";
 import { useLocation } from "react-router-dom";
 
 export const HomePage = () => {
+  const date = new Date().getFullYear();
   const [posts, setPosts] = useState([]);
   const { search } = useLocation();
   console.log();
@@ -28,12 +29,25 @@ export const HomePage = () => {
     <>
       <TopBar />
       <Header />
-      
+
       <div className="home">
         <Posts posts={posts} />
       </div>
       <div className="cat"></div>
-    
+      <h4 className="aboutus">Copyright &copy; Jenny & Sherin {date}</h4>
+      <h4 className="contacts">
+        <i> Jenny Fiskaare</i> <i className="fab fa-linkedin-in"></i>{" "}
+        <i className="fa-brands fa-github"></i>
+      </h4>
+      <h4 className="contacts">
+        <i>Sherin Susan Thomas</i>
+        <a href="https://www.linkedin.com/in/dr-sherin-thomas/">
+          <i className="fab fa-linkedin-in"></i>
+        </a>
+        <a href="https://github.com/Sherin-Susan-Thomas?tab=repositories">
+          <i className="fa-brands fa-github"></i>
+        </a>
+      </h4>
     </>
   );
 };
--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -128,7 +128,6 @@ export const Profile = () => {
         <p>{errorMessage}</p>
         <p>{successMessage}</p>
       </div>
-
     </div>
   );
 };
